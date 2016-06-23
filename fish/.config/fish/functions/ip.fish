bdc1e2e556b6e1a972e87f6f48a1590468d85129
Svein Olav Risdal <svein.olav.risdal@gmail.com>
2016-06-23 08:16:17 +0200

    The `ip` function should now work for both internal and external ip addresses on linux machines and mac os x.

--- fish/.config/fish/functions/ip.fish
+++ fish/.config/fish/functions/ip.fish
@@ -1,3 +1,26 @@
 function ip -d "Display IP-address"
-  ipconfig getifaddr en0
+  set -l param "param not given"
+
+  if test $argv[1]
+    set param $argv[1]
+  end
+
+  switch $param
+  case "-i" "--internal" "local"
+    if test -e ipconfig
+      ipconfig getifaddr en0
+    else
+      ifconfig wlan0 | grep Bcast | awk '{print $2; exit}' | sed s/addr://
+    end
+  case "-e" "--external" "www"
+    dig +short myip.opendns.com @resolver1.opendns.com
+  case "*"
+    echo "Usage: ip [PARAM]"
+    echo "Display either the internal or external ip address. "
+    echo ""
+    echo " -i, --internal             Display the internal ip address"
+    echo " -e, --external             Display the external ip address"
+    echo " local                      An alias for --internal"
+    echo " www                        An alias for --external"
+  end
 end
